1d6ee9692f92b2c50182c13b82c4ca9825438e64
Jennifer Nguyen <42787203+jenniferngp@users.noreply.github.com>
2026-08-02 21:05:05 -0700

    Update 01.mkref.sh

--- 10xmultiome/work/01.mkref.sh
+++ 10xmultiome/work/01.mkref.sh
@@ -2,7 +2,7 @@
 # 2. "transcript" should precede "exon" entries
 export PATH=/dfs6b/pub/jennipn6/software/cellranger-arc-2.1.0:$PATH
 
-cellranger-arc mkref --nthreads=14 --config=/dfs8/imarazzi_lab/share/jennifer/utmb/nhp/multiome/run2/processed/work/00.config.sh
+cellranger-arc mkref --nthreads=14 --config=/dfs8/imarazzi_lab/share/jennifer/multiome/run2/processed/work/00.config.sh
 
 tree
 
@@ -38,14 +38,14 @@ tree
 # │       ├── sjdbList.fromGTF.out.tab
 # │       ├── sjdbList.out.tab
 # │       └── transcriptInfo.tab
-# ├── Mmul_10-ebola_mayinga_zaire_combined_genome.fixed.gtf
-# └── Mmul_10-ebola_mayinga_zaire_combined_genome.fixed.sorted.gtf
+# ├── Mmul_10.fixed.gtf
+# └── Mmul_10.fixed.sorted.gtf
 
 # 5 directories, 29 files
 
 # compress
-cmd="tar -cvzf Mmul10_ZEBOV.tar.gz Mmul10_ZEBOV"
+cmd="tar -cvzf Mmul10.tar.gz Mmul10"
 echo $cmd; eval $cmd
 
-cmd="rclone copy local:Mmul10_ZEBOV.tar.gz 'dropbox:/14. Lab Members/jennifer/reference/personal/cellranger_reference' --no-gzip-encoding --transfers 4 --multi-thread-streams 4 --progress"
+cmd="rclone copy local:Mmul10.tar.gz 'dropbox:/14. Lab Members/jennifer/reference/personal/cellranger_reference' --no-gzip-encoding --transfers 4 --multi-thread-streams 4 --progress"
 echo $cmd; eval $cmd
